8a5bdc6a023f2e14f26b79849abd69f7b0848bfe
Zachary Chandler <zachary.c.me@gmail.com>
2022-01-30 17:29:29 -0800

    update port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
-
+const port = 4000
 
 const app = express()
 app.get("/", (request, response) => {
   response.send(["dog","bog","sog","flog"])
 })
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("EXPRESS IS AWAITING YOU")
 })
