124c04d4b408d1167dae93c8dd43508292906aa0
Zachary Chandler <zachary.c.me@gmail.com>
2022-01-30 17:25:41 -0800

    sends basic array

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const { response } = require("express")
 const express = require("express")
 
 
 const app = express()
-app.get("/"), (request, response) => {
+app.get("/", (request, response) => {
   response.send(["dog","bog","sog","flog"])
-}
+})
 
 app.listen(4000, () => {
   console.log("EXPRESS IS AWAITING YOU")
