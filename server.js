88facd290812046d4d04d3ea564e2b8fbfc3ae47
Zarchan <zachary.c.me@gmail.com>
2022-01-30 17:10:12 -0800

    listens on port 4000

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const { response } = require("express")
 const express = require("express")
+
+
+const app = express()
+app.get("/"), (request, response) => {
+  response.send(["dog","bog","sog","flog"])
+}
+
+app.listen(4000, () => {
+  console.log("EXPRESS IS AWAITING YOU")
+})
